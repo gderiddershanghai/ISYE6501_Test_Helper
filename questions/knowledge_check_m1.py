3c5d24a00f55212e919a6c01153dd0c1813572b0
Ginger de Ridder <ginger_de_ridder@hotmail.com>
2023-11-29 14:01:24 +0800

    added kc

--- questions/knowledge_check_m1.py
+++ questions/knowledge_check_m1.py
@@ -1,19 +1,113 @@
 
-check_1_q1 = 1
-check_1_q2 = 1
-check_1_q3 = 1
-check_1_q4 = 1
-check_1__q5 = 1
-check_1__q6 = 1
-check_1_q7 = 1
-check_1_q8 = 1
-check_1_q9 = 1
-check_1_q10 = 1
-check_1_q11 = 1
-check_1_q12 = 1
-check_1_q13 = 1
-check_1_q14= 1
-check_1_q15= 1
+check_1_q1 = """
+A survey of 25 people recorded each person’s family size and type of car.
+1. Which of these is a data point?
+-The 14th person’s family size and car type
+-The 14th person’s family size
+-The car type of each person
+"""
+
+check_1_q2 = """
+2. Which of these is structured data?
+-The contents of a person’s Twitter feed
+-The amount of money in a person’s bank account
+
+3. Which of these is time series data?
+-The average cost of a house in the United States every year since 1820
+-The height of each professional basketball player in the NBA at the start of the season
+"""
+check_1_q3 = """
+When comparing models, if we use the same data to pick the best model as we do to estimate how good the best one is, what is likely to happen?
+-The model will appear to be better than it really is.
+-The model will appear to be worse than it really is.
+-The model will appear to be just as good as it really is.
+"""
+
+check_1_q4 = """
+In k-fold cross-validation, how many times is each part of the data used for training, and for validation?
+-k times for training, and k times for validation
+-1 time for training, and k-1 times for validation
+-k-1 times for training, and 1 time for validation
+
+"""
+
+check_1__q5 = """
+The k-means algorithm for clustering is a “heuristic” because…
+…it runs quickly.
+…it never gets the best answer.
+…it isn’t guaranteed to get the best answer.
+"""
+
+check_1__q6 = """
+Which of these is generally a good reason to remove an outlier from your data set?
+The outlier is an incorrectly-entered data, not real data.
+Outliers like this only happen occasionally.
+"""
+
+check_1_q7 = """
+In the CUSUM model, having a higher threshold T makes it…
+…detect changes faster, and less likely to falsely detect changes.
+…detect changes faster, and more likely to falsely detect changes.
+…detect changes slower, and less likely to falsely detect changes.
+…detect changes slower, and more likely to falsely detect changes.
+
+"""
+
+check_1_q8 = """
+A multiplicative seasonality, like in the Holt-Winters method, means that the seasonal effect is…
+- The same regardless of the baseline value.
+- Proportional to the baseline value.
+
+"""
+check_1_q9 = """
+Is exponential smoothing better for short-term forecasting or long-term forecasting?
+Short-term
+Long-term
+
+"""
+check_1_q10 = """
+Why is GARCH different from ARIMA and exponential smoothing?
+- GARCH uses time series data
+- GARCH is autoregressive
+- GARCH estimates variance
+"""
+check_1_q11 = """
+When would regression be used instead of a time series model?
+- When there are other factors or predictors that affect the response
+- When only previous values of the response affect its current value
+
+"""
+check_1_q12 = """
+If two models are approximately equally good, measures like AIC and BIC will favor the simpler model. Simpler models are often better because…
+-Simple models are easier to explain and “sell” to managers and executives
+-The effects observed in simple models are easier for everyone, including analytics professionals, to understand
+-Simple models are less likely to be over-fit to random effects
+-All of the above
+
+
+"""
+check_1_q13 = """
+What does “heteroscedasticity” mean?
+-The variance is different in different ranges of the data correct
+-The variances of two samples of data are different from each other
+
+"""
+check_1_q14= """
+A model is built to determine whether data points belong to a category or not. A “true negative” result is:
+- A data point that is in the category, but the model incorrectly says it isn’t.
+- A data point that is not in the category, but the model incorrectly says it is.
+- A data point that is in the category, and the model correctly says it is.
+- A data point that is not in the category, and the model correctly says so.
+- A “Debbie Downer” (someone who often says negative things that bring down everyone’s mood).
+
+"""
+check_1_q15= """
+A common rule of thumb is to stop branching if a leaf would contain less than 5 percent of the data points. Why not keep branching and allow models to find very close fits to each very small subset of data?
+-Actually, that sounds like a great idea – we should keep branching and let models find very close fits to very small subsets of data!
+-Fitting to very small subsets of data will cause overfitting.
+-Fitting to very small subsets of data will make the tree have too many leaves.
+
+"""
 
 
 KNOWLEDGE_1_QUESTIONS = [check_1_q1, check_1_q2, check_1_q3, check_1_q4, check_1__q5, check_1__q6,
--- questions/knowledge_check_m2.py
+++ questions/knowledge_check_m2.py
@@ -1,19 +1,117 @@
 
-check_2_q1 = 1
-check_2_q2 = 1
-check_2_q3 = 1
-check_2_q4 = 1
-check_2_q5 = 1
-check_2_q6 = 1
-check_2_q7 = 1
-check_2_q8 = 1
-check_2_q9 = 1
-check_2_q10 = 1
-check_2_q11 = 1
-check_2_q12 = 1
-check_2_q13 = 1
-check_2_q14= 1
-check_2_q15= 1
+check_2_q1 = """
+Building simpler models with fewer factors helps avoid which problems?
+-Overfitting
+-Low prediction quality
+-Bias in the most important factors.
+-Difficulty of interpretation
+
+"""
+check_2_q2 = """
+When two predictors are highly correlated, which of the following statements is true?
+-Lasso regression will usually have positive coefficients for both predictors.
+-Ridge regression will usually have positive coefficients for both predictors.
+
+"""
+check_2_q3 = """
+If we’re testing to see whether red cars sell for higher prices than blue cars, we need to account for the type and age of the cars in our data set. This is called
+- Controlling
+- Comparing
+- Combining
+"""
+
+check_2_q4 = """
+In which of these situations is a factorial design more appropriate than A/B testing?
+- Choosing between two different banner ad designs for orange juice.
+- Picking the best-tasting of four different brands of orange juice.
+- Finding the best combination of factors in orange juice to maximize sales.
+
+"""
+
+check_2_q5 = """
+Which of these is a way that multi-armed bandit models deal with balancing exploration and exploitation?
+- As we get more sure of the best answer, we’re more likely to choose to use it.
+- If we’re unsure of the best answer, we should pick one and stick with it.
+- As we get more sure of the best answer, we’re more likely to try many different ones just to make sure.
+
+"""
+
+check_2_q6 = """
+1. If the time between customer arrivals to a restaurant at lunchtime fits the exponential distribution, then which of the following is true?
+- The number of arrivals per unit time follows the Weibull distribution.
+- The number of arrivals per unit time follows the Poisson distribution.
+
+"""
+
+check_2_q7 = """
+Which of these is a queuing model not appropriate for?
+- Determining the average wait time on a customer service hotline.
+- Estimating the length of the checkout lines at a grocery store.
+- Predicting the number of customers who will come to a restaurant tomorrow.
+
+"""
+
+check_2_q8 = """
+In analytics modeling, we need to…
+…try to avoid our biases affecting our models.
+…report our results honestly.
+…be open to other people’s models, even if they’re different from ours.
+…develop our own intuition and artistry.
+"""
+
+check_2_q9 = """
+Which of these is a common reason that data sets are missing values?
+-A person accidentally typed in the wrong value.
+-A person did not want to reveal the true value.
+-An automated system did not work correctly to record the value.
+-All of the above.
+"""
+
+check_2_q10 = """
+Which statements are true about data imputation?
+Imputing more than 5 percent of values is usually not recommended.
+Imputation of more than one factor value for a data point is also possible.
+Both answers above are correct.
+"""
+
+check_2_q11 = """
+Which of these statistical models does not have an underlying optimization model to find the best fit?
+Linear regression
+Logistic regression
+Lasso regression
+Exponential smoothing
+k-means clustering
+None of the above
+
+"""
+
+check_2_q12 = """
+The two main steps of most optimization algorithms are:
+- Find the most important remaining variable, and assign its value to be as large as possible.
+- Find a good direction to move from the current solution, and determine how far to go in that direction.
+- Pick a set of variables to be zero, and find the best values of the remaining variables.
+
+"""
+check_2_q13 = """
+Nonparametric tests are useful when…
+- we don’t know much about the form of the underlying distribution the data comes from, or it doesn’t fit a nice distribution.
+- it’s important to have information about the median.
+- we don’t have much data.
+- we want to know whether the means of two distributions are similar.
+
+"""
+check_2_q14= """
+Suppose we have a graph where all edge weights are equal to 1. In the video, we saw how to split a graph up into highly-interconnected communities. Now, instead we want to split the nodes into large groups that have very few connections between them (for example, if a marketer wants to find sets of people in a social network who probably have very different sets of friends). How might you do that?
+- Change the Louvain algorithm to minimize modularity instead of maximizing it.
+- Change the graph: for every pair of nodes i and j, if there’s an edge between i and j then remove it; and if there’s not an edge between i and j, then add it. Then run the Louvain algorithm on the new graph.
+
+"""
+check_2_q15= """
+Deep learning is one of the current best approaches for which of these?
+Image recognition
+Splitting graphs into highly-connected communities
+Demand forecasting
+"""
 
 
 KNOWLEDGE_2_QUESTIONS = [check_2_q1, check_2_q2, check_2_q3, check_2_q4, check_2_q5, check_2_q6,
