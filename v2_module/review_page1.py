2256257bda92d35bf2e198059c1590e750322c4e
Ginger de Ridder <ginger_de_ridder@hotmail.com>
2024-09-04 23:15:16 +0800

    Quick commit

--- v2_module/review_page1.py
+++ v2_module/review_page1.py
@@ -26,63 +26,31 @@ def sa_questions():
     apply_custom_css()
     questions = st.session_state.token.mpc_questions
 
-    # for i, q in enumerate(questions, start=0):
-    #     label = q['question']
-    #     options = q['options_list']
-    #     correct_answer = q['correct_answer']
-    #     question_key = f"question_{i}"
-    #     explanation = q['explanation']
-
-    #     print('question key', question_key)
-
-    #     st.markdown('-------------------------------')
-    #     st.markdown(f'<div class="question-style">{label}</div>', unsafe_allow_html=True)
-
-    #     question = question_generator(label, options, question_key)
-
-    #     if st.button('Submit', key=f"submit_{i}"):
-    #         if question == correct_answer:
-    #             st.success('Great work!')
-    #             st.info(f'Explanation: \n\n{explanation}')
-    #         else:
-    #             st.error(f"The correct answer was {correct_answer}")
-    #             st.info(f'Explanation: \n\n{explanation}')
-
-    #         if 'chapter_information' in q:
-    #             st.write(f"You can review {q['chapter_information']}")
     for i, q in enumerate(questions, start=0):
-                label = q['question']
-                options = q['options_list']
-                correct_answer = q['correct_answer']
-                question_key = f"question_{i}"
-                explanation = q['explanation']
+        label = q['question']
+        options = q['options_list']
+        correct_answer = q['correct_answer']
+        question_key = f"question_{i}"
+        explanation = q['explanation']
 
-                st.markdown('-------------------------------')
-                # Use st.markdown to render the question, allowing LaTeX and markdown formatting
-                st.markdown(f"**{label}**")
+        print('question key', question_key)
 
-                question = question_generator(label, options, question_key)
+        st.markdown('-------------------------------')
+        st.markdown(label)
 
-                # Store submitted answers in session state
-                if st.button('Submit', key=f"submit_{i}"):
-                    if 'submitted_answers' not in st.session_state:
-                        st.session_state.submitted_answers = {}
+        question = question_generator(label, options, question_key)
 
-                    st.session_state.submitted_answers[question_key] = question
-                    print('question', question)
-                    print(correct_answer, 'corect')
-                    if question not in ['True', 'False']: question = question[0]
-                
+        if st.button('Submit', key=f"submit_{i}"):
+            if question == correct_answer:
+                st.success('Great work!')
+                st.info(f'Explanation: \n\n{explanation}')
+            else:
+                st.error(f"The correct answer was {correct_answer}")
+                st.info(f'Explanation: \n\n{explanation}')
 
-                    if question == correct_answer:
-                        st.success('Great work!')
-                        st.info(f'Explanation: \n\n{explanation}')
-                    else:
-                        st.error(f"The correct answer was {correct_answer}")
-                        st.info(f'Explanation: \n\n{explanation}')
-
-                    if 'chapter_information' in q:
-                        st.write(f"You can review {q['chapter_information']}")
+            if 'chapter_information' in q:
+                st.write(f"You can review {q['chapter_information']}")
+   
 
 
 
