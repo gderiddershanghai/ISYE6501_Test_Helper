1c5d7221c8dc56c7dd4f71f84467f23d66ac3cef
Ginger de Ridder <ginger_de_ridder@hotmail.com>
2024-09-15 23:21:47 +0800

    Quick commit

--- v2_app.py
+++ v2_app.py
@@ -23,7 +23,7 @@ If you find this app useful, please consider giving it a star on GitHub: [ISYE65
 Or check out my other review apps for:
 - [ISYE 6501 Introduction to Analytics Modeling](https://isye6501test-prep.streamlit.app/)
 - [MGT 6203 Analytics for Business](https://mgt-6203-mt-study-aid.streamlit.app/)
-- [ISYE 6740 Computational Data Analytics](https://www.google.com)
+- [ISYE 6740 Computational Data Analytics](https://cda-review-app.streamlit.app/)
 - [CS 7643 Deep Learning](https://deep-learning-practice.streamlit.app/)
 - [CS 7280 Network Science Review App](https://network-science-review.streamlit.app/)
 
